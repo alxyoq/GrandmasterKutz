cdf75636ecaeef5952a0790c6abe8590867f8e52
alxyoq <alxyoq@gmail.com>
2026-08-06 17:30:10 -0400

    Configure static Next.js export for Netlify

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   agentRules: false,
+  output: "export",
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
   reactStrictMode: true,
   poweredByHeader: false,
 };
